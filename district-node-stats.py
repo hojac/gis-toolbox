27a51757cae0d608ce0b7fe514616d82e628f0ea
David Kolossa <david.kolossa@rwth-aachen.de>
2016-01-09 18:28:04 +0100

    Configuration to print statistics to json file

--- district-node-stats.py
+++ district-node-stats.py
@@ -5,8 +5,6 @@ from shapely.geometry import shape, Point
 import json
 import urllib.request, urllib.error
 from matplotlib import pyplot
-from shapely.geometry import MultiPolygon
-from descartes.patch import PolygonPatch
 
 from mapplot import plot_polygon, plot_coord_scatter
 
@@ -25,6 +23,10 @@ DISTRICT_NAME_PROPERTY = "ST_NAME"
 # The location of the nodes.json
 NODES_JSON_URI = "http://freifunk-aachen.de/map/nodes.json"
 NODES_JSON_PATH = "data/ffac-nodes.json"
+
+# The output path of the statistics (json format)
+# Use None to print to stdout
+STATISTICS_OUTPUT_PATH = "data/ffac-district-statistics.json"
 
 # The output path for the plotted map
 # Set to None for interactive output
@@ -67,8 +69,6 @@ with open(NODES_JSON_PATH, "r") as f:
 			withgeo += 1
 		else:
 			nogeo += 1
-print("Nodes with coordinates: ", withgeo)
-print("Nodes without coordinates: ", nogeo)
 
 
 # Tally nodes within the individual district shapes
@@ -77,10 +77,30 @@ for _,node in coords.items():
 		if shape.contains(node):
 			district_stats[district] += 1
 
-print(json.dumps(district_stats, sort_keys=True, indent=4, separators=(",", ":"), ensure_ascii=False))
-print("Sum of nodes with coordinates within city limits: ", sum([x for _,x in district_stats.items()]))
+
+# Set up object to dump to json
+data_dump = {}
+data_dump["counts"] = {}
+data_dump["counts"]["with_geo"] = withgeo
+data_dump["counts"]["without_geo"] = nogeo
+data_dump["counts"]["within_boundary"] = sum([x for _,x in district_stats.items()])
+data_dump["districts"] = district_stats
 
 
+# Output statistics as json
+json_dump = json.dumps(data_dump, sort_keys=True, indent=4, separators=(",", ":"), ensure_ascii=False)
+if STATISTICS_OUTPUT_PATH is not None:
+	try: 
+		with open(STATISTICS_OUTPUT_PATH, "w") as f:
+			f.write(json_dump)
+			f.write("\n")
+	except IOError:
+		print("Could not write JSON to ", STATISTICS_OUTPUT_PATH)
+else:
+	print(json_dump)
+
+
+# Prepare visualization plot
 fig = pyplot.figure(1, dpi=100)
 ax = fig.add_subplot(111)
 ax.set_ylim(MAP_BOUNDS_LONG)
@@ -88,13 +108,16 @@ ax.set_xlim(MAP_BOUNDS_LAT)
 ax.set_title(MAP_TITLE)
 
 
+# Plot data
 for _,polygon in district_shapes.items():
 	plot_polygon(ax, polygon, color="#000000", alpha=1, linewidth=1)
 
 plot_coord_scatter(ax, [node for _,node in coords.items()], color="#ff0000", alpha=0.5)
 
+
+# Output plot to either file or interactive/zoomable window
 if MAP_OUTPUT_PATH is not None:
 	pyplot.savefig(MAP_OUTPUT_PATH)
 else:
 	pyplot.show()
-    
+
